7a2efce542c72f94e6d588980a540a7740dca17d
Katherine Aguirre <katherine.aguirre@kinandcarta.com>
2022-10-14 21:39:44 -0300

    chore: added home page, updated Readme, fixed home logo click

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,41 +7,35 @@ import RandMHome from '../../assets/img/RandMHome.png';
 export interface IHomePageProps {}
 
 const HomePage: FunctionComponent<IHomePageProps> = () => (
-    <CardContent data-testid="HomePagePage">
-      <ContentWrapperComponent>
-        <Card >
-          <Box>
-            <h1>Rick and Morty</h1>
-          </Box>
-          <CardMedia
-            component="img"
-            height="360"
-            image= {RandMHome}
-            alt="Rick and Morty"
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-                Rick and Morty is an American adult animated science-fiction sitcom created by Justin Roiland and Dan Harmon for Cartoon Networks nighttime programming block Adult Swim.
-                It is distributed internationally by Warner Bros. Domestic Television.
-            </Typography>
-            &nbsp;
-            <Typography variant="body2" color="text.secondary">
-              The series follows the misadventures of cynical mad scientist Rick Sanchez and his good-hearted but fretful grandson Morty Smith,
-              who split their time between domestic life and interdimensional adventures that take place across an infinite number of realities,
-              often travelling to other planets and dimensions through portals and on Rick flying saucer.
-            </Typography>
-            &nbsp;
-            <Typography variant="body2" color="text.secondary">
-              The general concept of Rick and Morty relies on two conflicting scenarios: domestic family drama, and an alcoholic grandfather dragging his grandson into high jinks.
-            </Typography>
-            &nbsp;
-            <Typography variant="h3" color="text.secondary">
-              This project is related with the Rick and Morty API and its application with React as UI tool.
-            </Typography>
-          </CardContent>
-        </Card>
-      </ContentWrapperComponent>
-    </CardContent>
-  );
+  <CardContent data-testid="HomePagePage">
+    <ContentWrapperComponent>
+      <Card>
+        <Box>
+          <h1>Rick and Morty</h1>
+        </Box>
+        <CardMedia component="img" height="360" image={RandMHome} alt="Rick and Morty" />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Rick and Morty is an American adult animated science-fiction sitcom created by Justin Roiland and Dan Harmon for Cartoon Networks nighttime programming block Adult Swim. It is distributed internationally by Warner Bros. Domestic
+            Television.
+          </Typography>
+          &nbsp;
+          <Typography variant="body2" color="text.secondary">
+            The series follows the misadventures of cynical mad scientist Rick Sanchez and his good-hearted but fretful grandson Morty Smith, who split their time between domestic life and interdimensional adventures that take place across an
+            infinite number of realities, often travelling to other planets and dimensions through portals and on Rick flying saucer.
+          </Typography>
+          &nbsp;
+          <Typography variant="body2" color="text.secondary">
+            The general concept of Rick and Morty relies on two conflicting scenarios: domestic family drama, and an alcoholic grandfather dragging his grandson into high jinks.
+          </Typography>
+          &nbsp;
+          <Typography variant="h3" color="text.secondary">
+            This project is related with the Rick and Morty API and its application with React as UI tool.
+          </Typography>
+        </CardContent>
+      </Card>
+    </ContentWrapperComponent>
+  </CardContent>
+);
 
 export default HomePage;
